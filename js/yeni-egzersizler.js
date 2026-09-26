0d24e642ba898b4d6ab6a2f8fc015e255e87d76b
Ekmeksiz <86563283+Ekmeksiz@users.noreply.github.com>
2026-09-26 15:02:03 +0300

    Towel Row (Havlu kürek) egzersizi ekle

--- js/yeni-egzersizler.js
+++ js/yeni-egzersizler.js
@@ -7,5 +7,6 @@ export function yeniEgzersizler(E,T){
   return [
     // E('ornekId','Örnek egzersiz adı',{pattern:'Yatay itme',muscles:{gogus:1,triseps:.5},systems:{sinir:1,kasItme:.5},
     //   days:[1,4],targets:T([[3,10],[3,10],[3,11],[2,9]]),rpe:6,note:'...'}),
+    E('towelRow','Havlu kürek (Towel Row)',{pattern:'Yatay çekme',muscles:{lat:1,ustSirt:1,biseps:.5,onkol:1},systems:{kasCekme:1,tendonKol:.5,sinir:.5},days:[1],targets:T([[3,8],[3,9],[3,10],[2,8]]),rpe:6,note:'Havluyu sabit bir yere (kapı, direk) sıkıca sar, geriye yaslan, vücut düz. Kürek kemiklerini sıkarak göğsü havluya çek, yavaş bırak.'}),
   ];
 }
